a40af8de3908c2eaba96cae9f84c94c5060b2959
sandersb725 <136634756+sandersb725@users.noreply.github.com>
2024-12-29 16:28:59 -0500

    Update hivebot.py

--- hivebot.py
+++ hivebot.py
@@ -3,14 +3,18 @@ import scratchconnect
 from hugchat import hugchat
 from hugchat.login import Login
 
-EMAIL = "bus120610@gmail.com"
-PASSWD = "N&YPr3kWr(6dVSm"
+# You do need to make an account at https://huggingface.co/ to proceed
+
+data_pool = 20 # amount of comments sampled
+
+EMAIL = "email" # change this to your huggingface email
+PASSWD = "password" # change this to your huggingface password
 cookie_path_dir = "./cookies/"
-sign = Login(EMAIL, PASSWD)
+sign = Login(EMAIL, PASSWD) 
 cookies = sign.login(cookie_dir_path=cookie_path_dir, save_cookies=True)
 
-login = scratchconnect.ScratchConnect("atoke-bf", "N&YPr3kWr(6dVSm")
-studio = login.connect_studio(studio_id=32147244)
+login = scratchconnect.ScratchConnect("username", "password") # change these to your scratch login
+studio = login.connect_studio(studio_id=32147244) # you should change the studio number to the studio you want the bot to deploy in
 
 chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
 
@@ -18,10 +22,9 @@ while True:
     studio.update_data()
     comments = []
 
-    for x in range(20):
-        comments.append(studio.comments(all=False, limit=20, offset=0)[0][x]['content'])
+    for x in range(data_pool):
+        comments.append(studio.comments(all=False, limit=data_pool, offset=0)[0][x]['content'])
     
     init = f"Speak to me and act like the comments proceeding: {comments}. Make sure you're message is short. Dont use too many caps. Try not to copy other comments. Be as funny as possible. Please avoid serious topics like venting. Be as enthusiatsic and over the top. Don't be too relatable."
-    #print(chatbot.chat(init))
     studio.post_comment(content=f"bot generated : {chatbot.chat(init)}")
     time.sleep(45)
